6e6b8c28986b14005753769319c8db15e144949e
Alexis <alexis.rieucau@ynov.com>
2020-12-18 17:44:35 +0100

    Remove spaces

--- build.js
+++ build.js
@@ -19,7 +19,7 @@ builder.build({
     targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
     config: {
         appId: 'EverShellLauncher',
-        productName: 'EverShell Launcher',
+        productName: 'EverShell-Launcher',
         artifactName: '${productName}-setup-${version}.${ext}',
         copyright: 'Copyright © 2020-2021 Beldah',
         directories: {
